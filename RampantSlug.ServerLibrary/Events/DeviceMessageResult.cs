628eeac91e3760c50d23af3f4c39fbdc8063a593
Lachlan Dowd <lachlan.dowd@gmail.com>
2015-03-23 07:46:53 +1100

    added missing files from source control

---
+++ RampantSlug.ServerLibrary/Events/DeviceMessageResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using RampantSlug.Common.Devices;
+
+namespace RampantSlug.ServerLibrary.Events
+{
+    public class DeviceMessageResult
+    {
+        
+        
+        
+        public DateTime Timestamp { get; set; }
+
+        public Switch Device { get; set; }  // TODO: Change this to a higher level IDevice that switch/coil etc... inherits from
+    }
+}
