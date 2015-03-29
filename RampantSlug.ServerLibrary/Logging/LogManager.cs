be751c120181a7e09e96cef2f4e7980f1f6f14b7
Lachlan Dowd <lachlan.dowd@gmail.com>
2015-03-29 11:18:03 +1100

    Added logging and created interface for DeviceTree

---
+++ RampantSlug.ServerLibrary/Logging/LogManager.cs
@@ -0,0 +1,40 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RampantSlug.ServerLibrary.Logging
+{
+    public class RsLogManager
+    {
+        //public static ILog GetCurrent
+
+        private IServerBusController _busController;
+
+        private static readonly Lazy<RsLogManager> ClassInstance = new Lazy<RsLogManager>(() => new RsLogManager());
+
+        private RsLogManager()
+	    {
+            
+	    }
+	
+        /// <summary>
+        /// Use the active logger for log message
+        /// </summary>
+        public static RsLogManager GetCurrent
+	    {
+		    get { return ClassInstance.Value; }
+	    }
+
+        public void LogTestMessage(string message)
+        {
+            if (_busController == null)
+            {
+                _busController = IoC.Get<IServerBusController>();
+            }
+            _busController.SendEventMessage(message);
+        }
+    }
+}
