4229fa062ddb4fb7cef52a10074e5faeb182ac89
lachlan.dowd <lachlan.dowd@gmail.com>
2015-04-23 11:41:03 +1000

    Some commenting of Common and Cleaned up History View of Devices to resolve #38

--- RampantSlug.Common/TimedAction.cs
+++ RampantSlug.Common/TimedAction.cs
@@ -7,18 +7,26 @@ using System.Windows.Threading;
 
 namespace RampantSlug.Common
 {
+    /// <summary>
+    /// Helper class to execute an action with a specified delay
+    /// </summary>
     public static class TimedAction
     {
+        /// <summary>
+        /// Execute an action after a set amount of time
+        /// </summary>
+        /// <param name="action">Action to perform</param>
+        /// <param name="delay">Time to wait before executing</param>
         public static void ExecuteWithDelay(Action action, TimeSpan delay)
         {
             DispatcherTimer timer = new DispatcherTimer();
             timer.Interval = delay;
             timer.Tag = action;
-            timer.Tick += new EventHandler(timer_Tick);
+            timer.Tick += new EventHandler(TimerTick);
             timer.Start();
         }
 
-        private static void timer_Tick(object sender, EventArgs e)
+        private static void TimerTick(object sender, EventArgs e)
         {
             DispatcherTimer timer = (DispatcherTimer) sender;
             Action action = (Action) timer.Tag;
---
+++ RampantSlug.ServerLibrary/Events/UpdateDeviceEvent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using RampantSlug.Common.Devices;
+
+namespace RampantSlug.ServerLibrary.Events
+{
+    public class UpdateSwitchEvent
+    {
+        public Switch Device { get; set; }
+    }
+
+    public class UpdateCoilEvent
+    {
+        public Coil Device { get; set; }
+    }
+
+    public class UpdateStepperMotorEvent
+    {
+        public StepperMotor Device { get; set; }
+    }
+
+    public class UpdateServoEvent
+    {
+        public Servo Device { get; set; }
+    }
+
+    public class UpdateLedEvent
+    {
+        public Led Device { get; set; }
+    }
+}
