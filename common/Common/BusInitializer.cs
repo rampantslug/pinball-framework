02d3c145775a237fadb5810ecaaa835d50195cf4
lachlan.dowd@gmail.com <lachlan.dowd@gmail.com>
2018-05-30 20:16:52 +1000

    Updated nuget packages

--- common/Common/BusInitializer.cs
+++ common/Common/BusInitializer.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GreenPipes;
 using MassTransit;
+using MassTransit.RabbitMqTransport;
 
 
 namespace Common
